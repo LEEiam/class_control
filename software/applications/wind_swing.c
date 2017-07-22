32e53eeeef8f5ece9526ae712ae1b9c814dc54d8
JianRuiqian <jianb1995@hotmail.com>
2017-07-22 16:15:37 +0800

    add swing mode 3, but not perfect

--- software/applications/wind_swing.c
+++ software/applications/wind_swing.c
@@ -9,15 +9,18 @@
 #endif
 
 #define PENDULUM_CYCLE      1108        /* 1108 ms, rod length: 0.307m */
-#define MACHINE_HEIGTH      49          /* heigth of the wind swing in cm */
+#define MACHINE_HEIGTH      49          /* heigth of the machine in cm */
 #define PI                  3.14159f    /* ¦° value in float type */
 #define RADIAN_TO_ANGLE     180 / PI    /* convert radian to angle */
 
 #define SAMPLE_INTERVAL     1000 / DEFAULT_MPU_HZ   /* sample interval in ms */
 #define OUTPUT_LIMIT        80          /* output limit for duty ratio in % */
-#define RADIUS_MIN          0.0f        /* max radius in cm */
-#define RADIUS_MAX          60.0f       /* min radius in cm */
+#define RADIUS_MIN          0.0f        /* min radius in cm */
+#define RADIUS_MAX          60.0f       /* max radius in cm */
 #define RADIUS_DEFAULT      30.0f       /* default radius in cm */
+#define ANGLE_MIN           0.0f        /* min angle */
+#define ANGLE_MAX           90.0f       /* max angle */
+#define ANGLE_DEFAULT       0.0f        /* default angle */
 
 #define INC_KP              0           /* Kp for incremental pid controller */
 #define INC_KI              0           /* Ki for incremental pid controller */
@@ -33,6 +36,7 @@ static struct rt_timer timer;
 static struct rt_semaphore sem;
 
 static float radius = RADIUS_DEFAULT;
+static float angle = ANGLE_DEFAULT;
 static rt_tick_t t = 0;
 
 static void swing_move(int duty_ratio_x, int duty_ratio_y)
@@ -89,7 +93,7 @@ void swing_mode_1(void)
     float duty_ratio_x;
     float duty_ratio_y;
     float theta;
-    float angle;
+    float angle_x;
     float set_pitch;
     float set_roll;
     
@@ -100,11 +104,11 @@ void swing_mode_1(void)
         rt_sem_take(&sem, RT_WAITING_FOREVER);
         
         /* calculate angle amplitude */
-        angle = atan(RADIUS_DEFAULT / MACHINE_HEIGTH) * RADIAN_TO_ANGLE;
+        angle_x = atan(RADIUS_DEFAULT / MACHINE_HEIGTH) * RADIAN_TO_ANGLE;
         /* calculate current target radian */
         theta = t * (2 * PI / PENDULUM_CYCLE);
         /* calculate current target angle */
-        set_pitch = angle * sin(theta);
+        set_pitch = angle_x * sin(theta);
         set_roll  = 0.0f;
         /* fetch currnet euler angle */
         dmp_get_eulerangle(&el);
@@ -131,7 +135,7 @@ void swing_mode_2(void)
     float duty_ratio_x;
     float duty_ratio_y;
     float theta;
-    float angle;
+    float angle_x;
     float set_pitch;
     float set_roll;
     
@@ -142,11 +146,11 @@ void swing_mode_2(void)
         rt_sem_take(&sem, RT_WAITING_FOREVER);
         
         /* calculate angle amplitude */
-        angle = atan(radius / MACHINE_HEIGTH) * RADIAN_TO_ANGLE;
+        angle_x = atan(radius / MACHINE_HEIGTH) * RADIAN_TO_ANGLE;
         /* calculate current target radian */
         theta = t * (2 * PI / PENDULUM_CYCLE);
         /* calculate current target angle */
-        set_pitch = angle * sin(theta);
+        set_pitch = angle_x * sin(theta);
         set_roll  = 0.0f;
         /* fetch currnet euler angle */
         dmp_get_eulerangle(&el);
@@ -169,9 +173,45 @@ void swing_mode_2(void)
 
 void swing_mode_3(void)
 {
-    while(1)
+    struct euler_angle el;
+    float duty_ratio_x;
+    float duty_ratio_y;
+    float theta;
+    float angle_x;
+    float angle_y;
+    float set_pitch;
+    float set_roll;
+    
+    t = 0;
+    
+    while (1)
     {
         rt_sem_take(&sem, RT_WAITING_FOREVER);
+        
+        /* calculate angle amplitude */
+        angle_x = atan(radius * cos(angle) / MACHINE_HEIGTH) * RADIAN_TO_ANGLE;
+        angle_y = atan(radius * sin(angle) / MACHINE_HEIGTH) * RADIAN_TO_ANGLE;
+        /* calculate current target radian */
+        theta = t * (2 * PI / PENDULUM_CYCLE);
+        /* calculate current target angle */
+        set_pitch = angle_x * sin(theta);
+        set_roll  = angle_y * sin(theta);
+        /* fetch currnet euler angle */
+        dmp_get_eulerangle(&el);
+        /* calculate the output duty ratio */
+        duty_ratio_x = pid_incremental_ctrl(pid_x, set_pitch, el.pitch);
+        duty_ratio_y = pid_incremental_ctrl(pid_y, set_roll, el.roll);
+        swing_move(duty_ratio_x, duty_ratio_y);
+        
+    #ifdef RT_USING_ANOP
+        anop_upload_float(ANOP_FUNC_CUSTOM_1, &set_pitch, 1);
+        anop_upload_float(ANOP_FUNC_CUSTOM_2, &el.pitch, 1);
+        anop_upload_float(ANOP_FUNC_CUSTOM_3, &duty_ratio_x, 1);
+        
+//        anop_upload_float(ANOP_FUNC_CUSTOM_6, &set_roll, 1);
+//        anop_upload_float(ANOP_FUNC_CUSTOM_7, &el.roll, 1);
+//        anop_upload_float(ANOP_FUNC_CUSTOM_8, &duty_ratio_y, 1);
+    #endif
     }
 }
 
@@ -222,6 +262,14 @@ void swing_set_radius(float r)
     rt_enter_critical();
     if (RADIUS_MIN <= r && r <= RADIUS_MAX)
         radius = r;
+    rt_exit_critical();
+}
+
+void swing_set_angle(float a)
+{
+    rt_enter_critical();
+    if (ANGLE_MIN <= a && a <= ANGLE_MAX)
+        angle = a;
     rt_exit_critical();
 }
 
@@ -278,13 +326,23 @@ void swing_deinit(void)
 #ifdef RT_USING_FINSH
 #include <finsh.h>
 
-void cmd_swing_raduis(int argc, char *argv[])
+void cmd_swing_radius(int argc, char *argv[])
 {
     if (argc == 2)
-        swing_init(atof(argv[1]));
+        swing_set_radius(atof(argv[1]));
     else
         rt_kprintf("Usage: swradius <radius>\n");
 }
+MSH_CMD_EXPORT_ALIAS(cmd_swing_radius, swradius, set swing radius);
+
+void cmd_swing_angle(int argc, char *argv[])
+{
+    if (argc == 2)
+        swing_set_angle(atof(argv[1]));
+    else
+        rt_kprintf("Usage: swangle <angle>\n");
+}
+MSH_CMD_EXPORT_ALIAS(cmd_swing_angle, swangle, set swing angle);
 
 void cmd_swing_pid(int argc, char *argv[])
 {
